12ed4ec49f01de1e395c525d1ca3d5dea6cd9534
Srchenko <sergiodb952@gmail.com>
2021-10-20 22:30:00 -0300

    cubicbezierto

--- pruebasDelTatu/prueba1/src/scenes/hud.ts
+++ pruebasDelTatu/prueba1/src/scenes/hud.ts
@@ -6,20 +6,18 @@ export default class Hud extends Phaser.Scene {
     }
 
     preload() {
-        // this.load.image("cosa_verde", "assets/hud/cosa_verde.png");
-        // this.load.image("pausa", "assets/hud/pausa.png");
-        // this.load.image("sonido_1", "assets/hud/sonido_1.png");
-        // this.load.image("sonido_2", "assets/hud/sonido_2.png");
+        
     }
 
     mostrarHudPosta(key: string) {
-        let hudAMostrar = JSON.parse(localStorage.getItem(key)).layers;
+        let hudAMostrarLayers = JSON.parse(localStorage.getItem(key)).layers;
+        let hudAMostrar = JSON.parse(localStorage.getItem(key));
         let objetos = [];
         let scene = this;
 
-        if (hudAMostrar != null) {
-            Object.keys(hudAMostrar).forEach(key => {
-                let layer = hudAMostrar[key];
+        if (hudAMostrarLayers != null) {
+            Object.keys(hudAMostrarLayers).forEach(key => {
+                let layer = hudAMostrarLayers[key];
                 layer.content.forEach(element => {
                     let obj;
                     console.log(element.name);
@@ -30,8 +28,34 @@ export default class Hud extends Phaser.Scene {
                     obj.setDepth(layer.depth);
 
                     if (key == "hud_botones") {
+                        let follower = {tiempo: 0, x: 0, y: 0};
+
                         if (element.properties) {
-                            
+                            element.properties.forEach(prop => {
+                                if (prop.name == "animation_id") {
+                                    let value = hudAMostrar.animations[prop.value];
+                                    let path = new Phaser.Curves.Path(value[0].x, value[0].y);
+                                    
+                                    for(let i = 0; i < value.length; i+=3) {
+                                        if (i != value.length - 1) {
+                                            path.cubicBezierTo(value[i+3].x, value[i+3].y, value[i+1].x, value[i+1].y, value[i+2].x, value[i+2].y);
+                                        }
+                                    }
+
+                                    let graphics = this.add.graphics();
+
+                                    graphics.clear();
+                                    graphics.lineStyle(2, 0xffffff, 1);
+
+                                    path.draw(graphics);
+                                }
+                                if (prop.name == "animation_type") {
+                                    
+                                }
+                                if (prop.name == "action") {
+                                    
+                                }
+                            });
                         }
                         obj.setInteractive();
                         let tweenDelObj;
