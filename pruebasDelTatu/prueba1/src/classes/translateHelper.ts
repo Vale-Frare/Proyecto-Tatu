6c1562ff91afa410e1f2f3fad333df65ba3ce230
Vale-Frare <81550205+Vale-Frare@users.noreply.github.com>
2021-11-09 01:48:07 -0300

    minifix

--- pruebasDelTatu/prueba1/src/classes/translateHelper.ts
+++ pruebasDelTatu/prueba1/src/classes/translateHelper.ts
@@ -2,7 +2,7 @@ export class translateHelper {
     static cargarTraducciones(lang: string) {
         return new Promise((resolve, reject) => {
             let apiFede = `https://traduci-la.herokuapp.com/rest/translation?project_id=ckvhrn33h12221nyg31y1zv6f&lang=${lang}`;
-            let apiFrase = `https://voluminouslegalmeasurements.frasesegundo.repl.co/tatu/?lang=${lang}`;
+            let apiFrase = `https://voluminouslegalmeasurements.frasesegundo.repl.co/tatudb/?lang=${lang}`;
 
             fetch(apiFede).then((response) => {
                 return response.json();
